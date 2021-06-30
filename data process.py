21e34eea40f2d09123aca18ff1b33f2aca069470
Noha AlDossari <60599498+Noha66@users.noreply.github.com>
2021-06-30 19:52:54 +0300

    Initial commit

---
+++ data process.py
@@ -0,0 +1,155 @@
+
+"""
+Created on Wed June 30 9:00:00 2021
+
+@author: Nuha and Rahaf
+"""
+
+
+import pandas as pd
+
+
+# Drop columns with correlation above defined threshold
+def drop_correlated_cols(df, cols_features, threshold):
+    """
+    Return a new dataframe, df, with columns correlated above threshold removed.
+    The column removed is the right most one.
+    """
+    cols_to_remove = set()
+    correlations_abs = df[cols_features].corr().abs()
+    for i in range(len(correlations_abs.columns)):
+        for j in range(i):
+            if (correlations_abs.iloc[i, j] >= threshold) and (correlations_abs.columns[j] not in cols_to_remove):
+                colname = correlations_abs.columns[i]
+                if colname in df.columns:
+                    cols_to_remove.add(colname)
+                    df.drop(colname, axis=1, inplace=True)
+    return df
+
+
+# Load the entire data set
+df = pd.read_excel('cyclicDisc.xlsx', na_values=['na', 'nan'])
+print(df.shape)
+
+dfEnthalpy = pd.DataFrame(df)
+dfHeatCapacity = pd.DataFrame(df)
+dfEntropy = pd.DataFrame(df)
+dfBoiling = pd.DataFrame(df)
+dfMelting = pd.DataFrame(df)
+
+
+# Standard enthalpy dataframe
+# Drop the columns
+dfEnthalpy.drop(['NAME','SMILES','Boiling Point', 'Entropy' , 'Heat Capacity','Melting Point'], axis='columns', inplace=True)
+
+# Drop rows with na
+dfEnthalpy.dropna(subset = ["Standard Enthalpy"], inplace=True)
+print(dfEnthalpy.shape)
+
+# Drop columns that have only a single value (variance = 0)
+count_unique = dfEnthalpy.apply(pd.Series.nunique)
+cols_to_drop = count_unique[count_unique == 1].index
+dfEnthalpy.drop(columns=cols_to_drop, inplace=True)
+print(dfEnthalpy.shape)
+
+# Drop highly correlated features
+cols_features = dfEnthalpy.columns[0:]
+dfEnthalpy = drop_correlated_cols(dfEnthalpy, cols_features, 0.9)
+print(dfEnthalpy.shape)
+
+
+# Heat capacity dataframe
+# Drop the columns
+dfHeatCapacity.drop(['NAME','SMILES','Boiling Point', 'Entropy' , 'Standard Enthalpy','Melting Point'], axis='columns', inplace=True)
+
+# Drop rows with na
+dfHeatCapacity.dropna(subset = ["Heat Capacity"], inplace=True)
+print(dfHeatCapacity.shape)
+
+# Drop columns that have only a single value (variance = 0)
+count_unique = dfHeatCapacity.apply(pd.Series.nunique)
+cols_to_drop = count_unique[count_unique == 1].index
+dfHeatCapacity.drop(columns=cols_to_drop, inplace=True)
+print(dfHeatCapacity.shape)
+
+# Drop highly correlated features
+cols_features = dfHeatCapacity.columns[0:]
+dfHeatCapacity = drop_correlated_cols(dfHeatCapacity, cols_features, 0.9)
+print(dfHeatCapacity.shape)
+
+
+# Entropy dataframe
+# Drop the columns
+dfEntropy.drop(['NAME','SMILES','Boiling Point', 'Heat Capacity' , 'Standard Enthalpy','Melting Point'], axis='columns', inplace=True)
+
+# Drop rows with na
+dfEntropy.dropna(subset = ["Entropy"], inplace=True)
+print(dfEntropy.shape)
+
+# Drop columns that have only a single value (variance = 0)
+count_unique = dfEntropy.apply(pd.Series.nunique)
+cols_to_drop = count_unique[count_unique == 1].index
+dfEntropy.drop(columns=cols_to_drop, inplace=True)
+print(dfEntropy.shape)
+
+# Drop highly correlated features
+cols_features = dfEntropy.columns[0:]
+dfEntropy = drop_correlated_cols(dfEntropy, cols_features, 0.9)
+print(dfEntropy.shape)
+
+
+# Boiling point dataframe
+# Drop the columns
+dfBoiling.drop(['NAME','SMILES','Melting Point', 'Standard Enthalpy' , 'Entropy' , 'Heat Capacity'], axis='columns', inplace=True)
+
+# Drop rows with na
+dfBoiling.dropna(subset = ["Boiling Point"], inplace=True)
+print(dfBoiling.shape)
+
+# Drop columns that have only a single value (variance = 0)
+count_unique = dfBoiling.apply(pd.Series.nunique)
+cols_to_drop = count_unique[count_unique == 1].index
+dfBoiling.drop(columns=cols_to_drop, inplace=True)
+print(dfBoiling.shape)
+
+# Drop highly correlated features
+cols_features = dfBoiling.columns[0:]
+dfBoiling = drop_correlated_cols(dfBoiling, cols_features, 0.9)
+print(dfBoiling.shape)
+
+
+# Melting point dataframe
+# Drop the columns
+dfMelting.drop(['NAME','SMILES','Boiling Point', 'Standard Enthalpy' , 'Entropy' , 'Heat Capacity'], axis='columns', inplace=True)
+
+# Drop rows with na
+dfMelting.dropna(subset = ["Melting Point"], inplace=True)
+print(dfMelting.shape)
+
+# Drop columns that have only a single value (variance = 0)
+count_unique = dfMelting.apply(pd.Series.nunique)
+cols_to_drop = count_unique[count_unique == 1].index
+dfMelting.drop(columns=cols_to_drop, inplace=True)
+print(dfMelting.shape)
+
+# Drop highly correlated features
+cols_features = dfMelting.columns[0:]
+dfMelting = drop_correlated_cols(dfMelting, cols_features, 0.9)
+print(dfMelting.shape)
+
+
+
+# Save standard enthalpy dataset
+dfEnthalpy.to_csv('CyclicEnthalpyProcessed.csv', index = False)
+
+# Save heat capacity dataset
+dfHeatCapacity.to_csv('CyclicHeatCapacityProcessed.csv', index = False)
+
+# Save entropy dataset
+dfEntropy.to_csv('CyclicEntropyProcessed.csv', index = False)
+
+# Save melting point dataset
+dfMelting.to_csv('CyclicMeltingPointProcessed.csv', index = False)
+
+# Save boiling point dataset
+dfBoiling.to_csv('CyclicBoilingPointProcessed.csv', index = False)
---
+++ gp chemical.py
@@ -0,0 +1,71 @@
+
+"""
+Created on Thu Nov 14 21:45:27 2019
+
+@author: pm09n
+"""
+# data analysis
+import pandas as pd
+
+# display all columns
+pd.set_option('max.columns', None)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+from sklearn.ensemble import (RandomForestRegressor, GradientBoostingRegressor)
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+# ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+# set seed
+SEED = 123
+
+data = pd.read_csv('ENTROPY_processed.csv')
+
+# separate data into train and test
+X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['names', 'Formula ','entropy','SMILES',	'No.']), # X: Remove the target label column and ID (not needed)
+                                                    data['entropy'], # y: Read the target label alone
+                                                    test_size=0.15, # 15% testing, 85% training
+                                                    random_state=SEED) # Fix the seed to the random generator
+str.encode().decode()
+print(f"Shape of training features {X_train.shape}")
+print(f"Shape of test features {X_test.shape}")
+
+# Instantiate a Gaussian Process model
+kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
+
+# Fit to data using Maximum Likelihood Estimation of the parameters
+gp.fit(X_train, y_train)
+
+# Make the prediction on the meshed x-axis (ask for MSE as well)
+y_pred, sigma = gp.predict(X_test, return_std=True)
+
+# Plot the function, the prediction and the 95% confidence interval based on
+# the MSE
+predictions = gp.predict(X_test).flatten()
+print(gp)
+for p in predictions:
+    print(p)  
+
+r2 = r2_score(y_test, predictions)
+plt.scatter(y_test,predictions)
+plt.xlabel('Actual values')
+plt.ylabel('Predicted values')
+
+plt.plot(np.unique(y_test), np.poly1d(np.polyfit(y_test, predictions, 1))(np.unique(y_test)))
+
+plt.text(0.6, 0.5, 'R-squared = %0.2f' % r2)
+plt.show()
